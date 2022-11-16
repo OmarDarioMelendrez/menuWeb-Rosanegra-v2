4ccc35469e0ab97ab7e63b69327b003ca76c6bb2
Daro <melendrezomar2@gmail.com>
2022-11-16 08:38:01 -0300

    fix changes 16 nov

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,8 +3,6 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import BannerCelu from "../images/banner-celu.jpg"
-import BannerWeb from "../images/banner-web.jpg"
 
 import "../styles/menu.css"
 
@@ -24,8 +22,6 @@ const menu = () => (
           <button className="btn">Tragos</button>
         </Link>
       </div>
-      <img src={BannerCelu} id="baner__celu" alt="usar barbijo" />
-      <img src={BannerWeb} id="baner__web" alt="usar barbijo" />
     </section>
   </Layout>
 )
