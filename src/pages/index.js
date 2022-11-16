dd2951780b907868a0e41aeb35d577bc6700e6d2
Daro <melendrezomar2@gmail.com>
2022-11-16 08:30:05 -0300

    new changes 11 nov

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,6 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import BannerCelu from "../images/banner-celu.jpg"
-import BannerWeb from "../images/banner-web.jpg"
 
 
 import '../styles/menu.css'
@@ -25,9 +23,6 @@ const IndexPage = () => (
             <button className="btn">Tragos</button>
           </Link>
         </div>
-        <img src={BannerCelu} id="baner__celu" alt="usar barbijo" />
-      <img src={BannerWeb} id="baner__web" alt="usar barbijo" />
-
       </section>
 
   </Layout>
